28da3215c709250ebaf23718640abae000d9f48b
Sugumar <sugumar@kttelematic.com>
2025-02-06 00:31:50 +0530

    chore: add proxy config to override cors during development

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+	return {
+		plugins: [sveltekit()],
+		server: {
+			proxy: {
+				'/api': {
+					target: `${env['PUBLIC_API_URL']}`,
+					changeOrigin: true
+				}
+			}
+		}
+	};
 });
